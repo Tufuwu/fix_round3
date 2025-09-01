93f45d9809794a3fda850afc61f5bcb5b4e8597b
wu <wu@.com>
2025-09-01 20:35:56 +0800

    run dlenski/vpn-slice/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,57 +1,39 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+import sys, os
 try:
-    from setuptools import setup
+  from setuptools import setup
 except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+  from distutils.core import setup
 
+if sys.version_info < (3,3):
+    sys.exit("Python 3.3+ is required; you are using %s" % sys.version)
 
-from tastypie import __version__
+########################################
 
+version_py = os.path.join('vpn_slice', 'version.py')
 
-setup(
-    name='django-tastypie',
-    version=__version__,
-    description='A flexible & capable API layer for Django.',
-    author='Daniel Lindsley',
-    author_email='daniel@toastdriven.com',
-    url='https://github.com/django-tastypie/django-tastypie',
-    long_description=open('README.rst', 'r').read(),
-    packages=[
-        'tastypie',
-        'tastypie.utils',
-        'tastypie.management',
-        'tastypie.management.commands',
-        'tastypie.migrations',
-        'tastypie.contrib',
-        'tastypie.contrib.gis',
-        'tastypie.contrib.contenttypes',
-    ],
-    package_data={
-        'tastypie': ['templates/tastypie/*'],
-    },
-    zip_safe=False,
-    requires=[
-        'python_mimeparse(>=0.1.4, !=1.5)',
-        'dateutil(>=1.5, !=2.0)',
-    ],
-    install_requires=[
-        'python-mimeparse >= 0.1.4, != 1.5',
-        'python-dateutil >= 1.5, != 2.0',
-    ],
-    tests_require=['mock', 'PyYAML', 'lxml', 'defusedxml'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities'
-    ],
-)
+d = {}
+with open(version_py, 'r') as fh:
+    exec(fh.read(), d)
+    version_pep = d['__version__']
+
+########################################
+
+setup(name="vpn-slice",
+      version=version_pep,
+      description=("vpnc-script replacement for easy split-tunnel VPN setup"),
+      long_description=open('description.rst').read(),
+      author="Daniel Lenski",
+      author_email="dlenski@gmail.com",
+      extras_require={
+        "setproctitle": ["setproctitle"],
+        "dnspython": ["dnspython"],
+      },
+      install_requires=["setproctitle", "dnspython"],
+      license='GPL v3 or later',
+      url="https://github.com/dlenski/vpn-slice",
+      packages=["vpn_slice"],
+      include_package_data = True,
+      entry_points={ 'console_scripts': [ 'vpn-slice=vpn_slice.__main__:main' ] },
+      )
